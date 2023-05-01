e7afa2ca7f47a042a94b608b2fffbef347c09dc3
Dubyshkina <ek.kalyazina@gmail.com>
2023-05-01 16:16:56 +0300

    Did first task with local storage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,76 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { nanoid } from 'nanoid';
+import s from 'components/App.module.css';
+
+import Form from './Form/Form';
+import ContactsList from './ContactsList/ContactsList';
+import Filter from './Filter/Filter';
+
+class App extends Component {
+  state = {
+    contacts: [],
+    filter: '',
+  };
+  componentDidMount() {
+    const contactList = JSON.parse(localStorage.getItem('contacts'));
+    if (contactList) this.setState({contacts: contactList});
+  }
+  componentDidUpdate(_, prevState) {
+ if(this.state.contacts !== prevState.contacts) localStorage.setItem('contacts', JSON.stringify(this.state.contacts))
+  }
+  handleAddContact = event => {
+    event.preventDefault();
+    const name = event.target[0].value;
+    const number = event.target[1].value;
+  
+    this.setState(prevState => {
+      return {
+        contacts: [...prevState.contacts, { name, number, id: nanoid() }],
+      };
+     
+    });
+  };
+  
+
+  handelFilter = event => {
+    this.setState({ filter: event.target.value });
+  };
+
+  filteredContacts = () => {
+    const { contacts, filter } = this.state;
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(filter?.toLowerCase())
+    );
+  };
+
+  handleDeleteBtn = id => {
+    this.setState(prevState => {
+      return {
+        contacts: prevState.contacts.filter(contact => contact.id !== id),
+      };
+    });
+  };
+
+  render() {
+    return (
+      <div className={s.container}>
+        <div className={s.section}>
+        <h2>Phonebook</h2>
+        <Form
+          handleAddContact={this.handleAddContact}
+        />
+        </div>
+        <div className={s.section}>
+        <h2>Contacts</h2>
+        <Filter handelFilter={this.handelFilter} />
+        <ContactsList
+          contacts={this.filteredContacts()}
+          handleDeleteBtn={this.handleDeleteBtn}
+        ></ContactsList>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Form/Form.jsx
@@ -0,0 +1,46 @@
+import { Component } from 'react';
+import PropTypes from 'prop-types';
+import s from '../Form/Form.module.css';
+
+class Form extends Component {
+  
+  static propTypes = {
+    handleAddContact: PropTypes.func.isRequired,
+  };
+
+  render() {
+    return (
+      <>
+        <form onSubmit={this.props.handleAddContact} className={s.form}>
+          <label htmlFor="name" className={s.label}>
+            Name                                                                                                  
+            </label>
+             <input
+              className={s.input}
+              type="text"
+              name="name"
+              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+              required
+            />
+          
+          <label htmlFor="number" className={s.label}>
+            Number 
+            </label>
+             <input
+            className={s.input}
+              type="tel"
+              name="number"
+              pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+              title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+              required
+            />
+          
+          <button type="submit" className={s.btn}>Add to contacts</button>
+        </form>
+      </>
+    );
+  }
+}
+
+export default Form;
